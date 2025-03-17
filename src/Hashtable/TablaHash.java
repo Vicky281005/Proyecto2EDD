95f960e170308059b0b7ff81b77cbc840f880cd0
Vicky281005 <maria.almonte@correo.unimet.edu.ve>
2025-03-17 17:40:28 -0400

    Primer acercamiento con la tabla Hash

---
+++ src/Hashtable/TablaHash.java
@@ -0,0 +1,40 @@
+
+package Hashtable;
+
+/**
+ *
+ * @author NITRO V 15
+ */
+public class TablaHash {
+    
+    public static void main(String[] args){
+        MetodoHash h = new MetodoHash(7);
+        
+        h.agregar(38);
+    h.agregar(48);
+    h.agregar(98);
+    h.agregar(18);
+    h.agregar(1);
+    h.agregar(25);
+    h.agregar(78);
+    h.agregar(55);
+    h.agregar(16);
+    h.agregar(13);
+    h.agregar(3);
+    h.agregar(63);
+    h.agregar(67);
+    h.agregar(79);
+    h.agregar(75);
+    h.agregar(95);
+    h.agregar(5);
+    
+    h.imprimir();
+    
+    h.borrar(1);
+    h.borrar(3);
+    h.borrar(5);
+    
+    h.imprimir();
+    }
+    
+}

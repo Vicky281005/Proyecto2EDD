a1ba9c85dfe51e8c57a55a41044d5c9b983dc413
Vicky281005 <maria.almonte@correo.unimet.edu.ve>
2025-03-23 15:36:10 -0400

    Akinator funcional, falta
    Mostrar el arbol y tambien falta ponerlo en el boton

--- src/Interfaz/Json.java
+++ src/Interfaz/Json.java
@@ -23,25 +23,149 @@ import javax.swing.JOptionPane;
  * @author jmmor
  */
 public class Json {
-//     public static ListaEnlazada valoresDelJsonSinRepeticionCopia;
+     public static ListaEnlazada valoresDelJsonSinRepeticionCopia = new ListaEnlazada();
      public  static JsonArray listaEspecies;   
      public static ListaEnlazada clavesDelJson;
      public static Arbol arbolParaGrafo; // Variable estática para compartir entre métodos
        
-//     public static void obtenerListaClaveValorJson(){
-//          Nodo aux = valoresDelJsonSinRepeticionCopia.getpFirst();
-//          
-//          while (aux != null){
-//               String valorDelJson = String.valueOf(aux.getData());
-//               
-//               int seleccion = JOptionPane.showConfirmDialog(null, "¿Quieres continuar?", "Confirmación", JOptionPane.YES_NO_OPTION);
-//               boolean continuar = (seleccion == JOptionPane.YES_OPTION);
-//               JOptionPane.showMessageDialog(null, "hola");
-//
-//               System.out.println(seleccion);
-//               
-//           }
-//       }
+     public static void obtenerListaClaveValorJson(){
+         Nodo aux = valoresDelJsonSinRepeticionCopia.getpFirst();
+         NodoArbol raizParaArbol = new NodoArbol(aux.getData());
+         arbolParaGrafo.volverRaizNula();
+         arbolParaGrafo.setRaiz(raizParaArbol);
+         NodoArbol auxArbol = raizParaArbol;
+         
+         int preguntasRealizadas = 0;
+         
+         Boolean haSidoadivinado = false;
+          while (aux != null && !haSidoadivinado){
+               String valorDelJson = String.valueOf(aux.getData());
+//               NodoArbol auxArbol = new NodoArbol(aux.getData());
+               
+               
+               int seleccion = JOptionPane.showConfirmDialog(null, valorDelJson, "Det si su clave tiene esta caract", JOptionPane.YES_NO_OPTION);
+               
+               boolean esVerdaderaLaRespuestaDelUsuario = (seleccion == JOptionPane.YES_OPTION);
+               preguntasRealizadas++;
+               
+               Boolean especiesMatcheada = false;
+               
+               if (!especiesMatcheada){
+                for (var objectoEspecie : listaEspecies) {
+                     var listaEspecieClaveValor = objectoEspecie.getAsJsonObject().entrySet();
+                     for (var especieClaveValor : listaEspecieClaveValor) {
+                         JsonArray listaPreguntas = especieClaveValor.getValue().getAsJsonArray();
+                         
+                         
+                         // ------------------------------------------------- modificar valores de clave (agregar valores que faltan a dicha lista de cada clave)
+                         
+                         Nodo auxi = clavesDelJson.getpFirst();
+                         JsonArray nuevoArray = new JsonArray();
+
+                         while (auxi != null) {
+
+                             Boolean seEncuentra = false;
+                             Boolean valorAAgregar = false;
+                             if (String.valueOf(auxi.getData()).equalsIgnoreCase(String.valueOf(especieClaveValor.getKey()))) {
+                                 
+                                 Nodo auxilio = valoresDelJsonSinRepeticionCopia.getpFirst();
+                                 while (auxilio != null){
+                                    for (var preguntaObjeto : listaPreguntas) {
+                                        for (Entry<String, JsonElement> preguntaClaveValor : preguntaObjeto.getAsJsonObject().entrySet()) {
+                                            if (String.valueOf(auxilio.getData()).equalsIgnoreCase(String.valueOf(preguntaClaveValor.getKey()))) {
+                                                seEncuentra = true;
+                                                valorAAgregar = Boolean.valueOf(preguntaClaveValor.getValue().toString());
+                                            }
+                                        }
+                                    }
+                                    JsonObject nuevoElemento = new JsonObject();
+                                    nuevoElemento.addProperty(String.valueOf(auxilio.getData()), seEncuentra ? valorAAgregar : false);
+                                    nuevoArray.add(nuevoElemento);
+                                    auxilio = auxilio.getpNext();
+                                    seEncuentra = false;
+                                    valorAAgregar = false;
+                                 }
+                                 
+
+                             }
+                             auxi = auxi.getpNext();
+
+                         }
+                         especieClaveValor.setValue(nuevoArray);
+//                         JOptionPane.showMessageDialog(null, especieClaveValor.getKey() + nuevoArray);
+                         // -------------------------------------------------
+ //                        clavesDelJson.addLast(especieClaveValor.getKey());
+                         NodoArbol auxNodoComparativo = arbolParaGrafo.getRaiz();
+                         int largo = listaPreguntas.size();
+                         int contador = 0;
+                         Boolean ultimaPregunta = false;
+
+                         if (largo == preguntasRealizadas){     
+                             while (auxNodoComparativo != null) {
+                                 for (var preguntaObjeto : listaPreguntas) {
+                                     for (Entry<String, JsonElement> preguntaClaveValor : preguntaObjeto.getAsJsonObject().entrySet()) {
+                                         if (auxNodoComparativo == null) continue;
+//                                         JOptionPane.showMessageDialog(null, "// " + preguntaClaveValor.getKey());
+//                                         JOptionPane.showMessageDialog(null, "evaluare " + auxNodoComparativo.getData() + " de " + especieClaveValor.getKey());
+                                         if (!String.valueOf(preguntaClaveValor.getKey()).equalsIgnoreCase(String.valueOf(auxNodoComparativo.getData()))){
+                                              auxNodoComparativo = auxNodoComparativo.getHijoIzq();
+                                              ultimaPregunta = false;
+                                         }
+                                         
+                                        if (String.valueOf(preguntaClaveValor.getKey()).equalsIgnoreCase(String.valueOf(auxNodoComparativo.getData())) ){
+                                             especiesMatcheada = true;
+//                                             JOptionPane.showMessageDialog(null, especieClaveValor.getKey());
+                                             contador++;
+                                             
+                                             if (Boolean.parseBoolean(preguntaClaveValor.getValue().toString())){
+                                                 
+//                                         JOptionPane.showMessageDialog(null, "Nos vamos pa la derecha");
+                                                 auxNodoComparativo = auxNodoComparativo.getHijoDer();
+                                                 ultimaPregunta = true;
+                                             }else{
+//                                         JOptionPane.showMessageDialog(null, "Nos vamos pa la izquierda");
+                                                 auxNodoComparativo = auxNodoComparativo.getHijoIzq();
+                                                 ultimaPregunta = false;
+                                             }
+                                         }
+                                     }
+                                 }
+
+
+                             }
+                         }
+                         
+                         if (contador == largo && ultimaPregunta == esVerdaderaLaRespuestaDelUsuario) {
+                             int  respuestaDeHaSidoadivinado = JOptionPane.showConfirmDialog(null, "TU CARTA ERA ESTA? "+ especieClaveValor.getKey());
+                              haSidoadivinado = (respuestaDeHaSidoadivinado == JOptionPane.YES_OPTION);
+                              
+                              if (haSidoadivinado) {
+                                  JOptionPane.showMessageDialog(null, "Yey soy lo maisimo");
+                              }
+                         }
+ //                         JOptionPane.showMessageDialog(null, "Sali del condicional");
+                     }
+                }
+                   
+               }
+               
+               aux = aux.getpNext();
+               
+               if (aux != null){
+                   
+                   NodoArbol auxDelAuxArbol = new NodoArbol(aux.getData());
+                   
+                   if (esVerdaderaLaRespuestaDelUsuario){
+                        auxArbol.setHijoDer(auxDelAuxArbol);
+                   } else {
+                        auxArbol.setHijoIzq(auxDelAuxArbol);
+                   }
+                   auxArbol = auxDelAuxArbol;
+               }
+               
+               
+           }
+       }
     // Método encargado de cargar el JSON
     public static void cargarJson() {
         try {
@@ -76,7 +200,7 @@ public class Json {
                         for (var preguntaObjeto : listaPreguntas) {
                             for (Entry<String, JsonElement> preguntaClaveValor : preguntaObjeto.getAsJsonObject().entrySet()) {
                                 valoresDelJsonSinRepeticion.addLastIfItsNotInList(preguntaClaveValor.getKey());
-//                                valoresDelJsonSinRepeticionCopia.addLastIfItsNotInList(preguntaClaveValor.getKey());
+                                valoresDelJsonSinRepeticionCopia.addLastIfItsNotInList(preguntaClaveValor.getKey());
 //                                JOptionPane.showMessageDialog(null, "A ver ya lo aniadi");
                             }
                         }
@@ -85,7 +209,7 @@ public class Json {
                 
 
                 arbolParaGrafo.insertarCaracteristicas(valoresDelJsonSinRepeticion, true);
-//                Json.obtenerListaClaveValorJson();
+                Json.obtenerListaClaveValorJson();
 
                 JOptionPane.showMessageDialog(null, "JSON cargado exitosamente.", "Información", JOptionPane.INFORMATION_MESSAGE);
             }
